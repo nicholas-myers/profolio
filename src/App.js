0acc114f67926d51d7a43cff605618cf0681aa21
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2021-04-16 12:40:16 -0700

    added some projects, some info and some links, needs to be completed later

--- src/App.js
+++ src/App.js
@@ -201,31 +201,62 @@ function App() {
       <Skills darkTheme={darkTheme} />
       <div className={darkTheme ? "dark-hero hero" : "light-hero hero"}>
         <h2>Projects</h2>
+        <div>
+          <h3>Apollo</h3>
+          <div style={{display: "flex", justifyContent: "space-around"}}>
+            <a href="https://github.com/Lambda-School-Labs/Labs26-Apollo-FE-TeamB">Front-End Code</a>
+            <a href="https://github.com/Lambda-School-Labs/Labs26-Apollo-BE-TeamB">Back-End Code</a>
+          </div>
+          <p>I worked as part of a cross-functional team. The back-end is developed in the Java Spring framework. The Front-End uses React and Redux. Secured with Okta.</p>
+        </div>
+        <div>
+          <h3>Conway's Game of Life</h3>
+          <p>Solo Project. The visual representation of the mathematical simulation of cell lifecycles. I constructed the state management using the Immer library to create copies of an immutable state. I enacted an algorithm to follow the rules of Conway for simulation of cell lifecycles. I implemented the code base in the one-week release canvas time frame. Languages: React and Immer library.</p>
+        </div>
+        <div>
+          <h3>Spotify Suggester</h3>
+        </div>
+        <div>
+          <h3>The Family Scrapbook</h3>
+          <p>I worked on this website as part of a 3 person team. My responsibility was the "Start Here Page". I also helped in the design and implementation of the navigation and footer. This project was written in HTML and CSS.</p>
+        </div>
+        <div>
+          <h3>Easy Dungeons</h3>
+          <p>This is a personal project. A digital toolset for Dungeons and Dragons 5E. Current tools, dice roller and intitiative tracker. Front-End is wrtten using React. Currently in the process of structuring the Back-End API.</p>
+        </div>
+        <div><h3>NimblePath</h3>
+        <p>Full Stack. Currently working with a client to bring to life their SaaS. Front-End written in React. Back-End to be written in Java using the Spring framework.</p>
+        </div>
+        <div>
+          <h3>GEARformation: Division 2</h3>
+          <a target="_blank" href="https://gearformation.netlify.app/">LIVE</a>
+          <p>This is a personal project. The information for the game was spread out over many sites or spreadsheets found online. My goal was to compile that information into one site to make the information easier to find. In the process of converting to React and creating a Java API for the backend. Languages: HTML and CSS.</p>
+        </div>
       </div>
-      <footer className={darkTheme ? "dark" : "light-footer"}>
+      <footer className={darkTheme ? "dark" : "light-header"}>
         <a
-          className={darkTheme ? "dark" : "light-footer"}
+          className={darkTheme ? "dark" : "light-header"}
           target="_blank"
           href="https://icons8.com/icon/48127/hand"
         >
           Hand icon by Icons8
         </a>
         <a
-          className={darkTheme ? "dark" : "light-footer"}
+          className={darkTheme ? "dark" : "light-header"}
           target="_blank"
           href="https://icons8.com/icon/XBJfETMfZHpS/light"
         >
           Light icon by Icons8
         </a>
         <a
-          className={darkTheme ? "dark" : "light-footer"}
+          className={darkTheme ? "dark" : "light-header"}
           target="_blank"
           href="https://icons8.com/icon/44818/light-on"
         >
           Light On icon by Icons8
         </a>
         <a
-          className={darkTheme ? "dark" : "light-footer"}
+          className={darkTheme ? "dark" : "light-header"}
           target="_blank"
           href="https://www.pexels.com/photo/computer-keyboard-34153/"
         >
