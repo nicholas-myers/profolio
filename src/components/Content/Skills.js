d759d1999e666131539cebf2860d9c2d78fccb79
nicholas-myers <nicholas.myers.professional@gmail.com>
2021-12-11 09:23:47 -0800

    fix section styling

--- src/components/Content/Skills.js
+++ src/components/Content/Skills.js
@@ -3,22 +3,24 @@ import React from "react";
 function Skills({ darkTheme }) {
   return (
     <section className={darkTheme ? "dark-hero hero" : "light-hero hero"}>
-      <h2>Skills</h2>
-      <p>{`< HTML : XML : REACT >`}</p>
-      <p>{`{ CSS : SASS : LESS }`}</p>
-      <h2>Libraries</h2>
-      <p>{`<Material UI>`}</p>
-      <p>{`<Ant Design>`}</p>
-      <p>{`<JavaScript>`}</p>
-      <p>{`<React>`}</p>
-      <p>{`<React Router>`}</p>
-      <p>{`<React DatePicker>`}</p>
-      <p>{`<Redux>`}</p>
-      <p>{`<Axios>`}</p>
-      <p>{`<Java>`}</p>
-      <p>{`<Spring>`}</p>
-      <p>{`<Python>`}</p>
-      <p>{`<OOP>`}</p>
+      <div>
+        <h2>Skills</h2>
+        <p>{`< HTML : XML >`}</p>
+        <p>{`< React : Javascript >`}</p>
+        <p>{`{ CSS : SASS : LESS }`}</p>
+        <p>{`OOP = Java : C# : Python`}</p>
+        <p>SQL : SSMS</p>
+      </div>
+      <div>
+        <h2>Libraries</h2>
+        <p>{`<Material UI>`}</p>
+        <p>{`<Ant Design>`}</p>
+        <p>{`<React Router>`}</p>
+        <p>{`<React DatePicker>`}</p>
+        <p>{`<Redux>`}</p>
+        <p>{`<Axios>`}</p>
+        <p>{`<Spring>`}</p>
+      </div>
     </section>
   );
 }
